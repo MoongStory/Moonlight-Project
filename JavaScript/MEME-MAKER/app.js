d9d94ed0bf068374391101041800a2ff6bb968db
Moong <moongstory@gmail.com>
2024-11-18 22:05:07 +0900

    밈 메이커 #2.5

--- JavaScript/MEME-MAKER/app.js
+++ JavaScript/MEME-MAKER/app.js
@@ -1,3 +1,4 @@
+const modeBtn = document.querySelector(".mode-btn");
 const colorOptions = Array.from(
   document.getElementsByClassName("color-option")
 );
@@ -6,12 +7,13 @@ const color = document.querySelector(".color");
 
 const canvas = document.querySelector("canvas");
 const ctx /* context */ = canvas.getContext("2d");
-canvas.width = 800;
-canvas.height = 800;
+canvas.width = 400;
+canvas.height = 400;
 
 ctx.lineWidth = lineWidth.value;
 
 let isPainting = false;
+let isFilling = false;
 
 function onMouseMove(event) {
   if (isPainting === true) {
@@ -51,11 +53,28 @@ function onColorClick(event) {
   color.value = event.target.dataset.color;
 }
 
+function onModeClick(event) {
+  if (isFilling === true) {
+    isFilling = false;
+    modeBtn.innerText = "Fill";
+  } else {
+    isFilling = true;
+    modeBtn.innerText = "Draw";
+  }
+}
+
+function onCanvasClick(event) {
+  if (isFilling === true) {
+    ctx.fillRect(0, 0, 400, 400);
+  }
+}
+
 // region 마우스 이벤트
 canvas.addEventListener("mousemove", onMouseMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
+canvas.addEventListener("click", onCanvasClick);
 // end-region 마우스 이벤트
 
 lineWidth.addEventListener("change", onLineWidthChange);
@@ -63,3 +82,5 @@ lineWidth.addEventListener("change", onLineWidthChange);
 color.addEventListener("change", onColorChange);
 
 colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
+
+modeBtn.addEventListener("click", onModeClick);
